ffc33112f8c1761ac8f819350668a8eb38356aab
Blaize Kaye <blaize.kaye@amazee.io>
2025-02-19 13:07:27 +1300

    updates volume mount

---
+++ .lagoon/assets/settings.php
@@ -0,0 +1,61 @@
+<?php
+
+/**
+ * @file
+ * Lagoon Drupal settings file.
+ *
+ * The primary settings file for Drupal which includes
+ * settings.lagoon.php that contains Lagoon-specific settings.
+ */
+
+/**
+ * Load services definition file.
+ */
+$settings['container_yamls'][] = __DIR__ . '/services.yml';
+
+/**
+ * Include the Lagoon-specific settings file.
+ *
+ * N.B. The settings.lagoon.php file makes some changes
+ *      that affect all environments that this site
+ *      exists in.  Always include this file, even in
+ *      a local development environment, to ensure that
+ *      the site settings remain consistent.
+ */
+include __DIR__ . "/settings.lagoon.php";
+
+/**
+ * Skipping permissions hardening.
+ *
+ * Enabling this will make scaffolding work better
+ * but will also raise a warning when you install Drupal.
+ * See https://www.drupal.org/project/drupal/issues/3091285.
+ *
+ * @code
+ *  $settings['skip_permissions_hardening'] = TRUE;
+ * @endcode
+ */
+
+// Last: this servers specific settings files.
+if (file_exists(__DIR__ . '/settings.local.php')) {
+  include __DIR__ . '/settings.local.php';
+}
+
+// Last: This server specific services file.
+if (file_exists(__DIR__ . '/services.local.yml')) {
+  $settings['container_yamls'][] = __DIR__ . '/services.local.yml';
+}
+$databases['default']['default'] = array (
+  'database' => 'drupal',
+  'username' => 'drupal',
+  'password' => 'drupal',
+  'prefix' => '',
+  'host' => 'mariadb',
+  'port' => 3306,
+  'isolation_level' => 'READ COMMITTED',
+  'driver' => 'mysql',
+  'namespace' => 'Drupal\\mysql\\Driver\\Database\\mysql',
+  'autoload' => 'core/modules/mysql/src/Driver/Database/mysql/',
+);
+$settings['hash_salt'] = 'K4-CJAJxpBuPy9kUU_soi1wyoXmsAKv1MmJELIrCrLe18vS2NsNV_JWm7sPBFEaMZkaDoVFhMA';
+$settings['config_sync_directory'] = 'sites/default/files/config_tBIFak_MAgupEGCOCduDpFmCE-cNaFIJN3XZuQj1RAX-F-TotuXvhhDv18YyWySehyoKLE0vxg/sync';
